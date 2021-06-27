3b86d298a25ce30341bf0a165ac7681ea5ae98a1
Swatantra Sinha <swatantrasinha15aug@gmail.com>
2021-06-27 11:30:37 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  const element = document.createElement('h1')
+  element.innerHTML = "This Sample Webpack is deployed in heroku successfully !!!"
+  document.body.appendChild(element)
+})
